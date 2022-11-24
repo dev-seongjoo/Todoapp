19586acfb63517416bf9b7eb91feb0723c8b52c0
seongjoo <dev.seongjoo@gmail.com>
2022-11-24 14:24:32 +0900

    update & delete post

--- server.js
+++ server.js
@@ -29,12 +29,11 @@ app.get('/write', (req,res) => {
   res.render('write.njk', {title: 'Write'});
 });
 
-app.post('/add', async (req, res) => {
-    console.log(req.body)
+app.post('/write', async (req, res) => {
     const result = await db.collection('counter').findOne({name: 'postNumber'});
     const totalPost = result.totalPost;
-    db.collection('post').insertOne({_id: totalPost+1,title: req.body.title, date: req.body.date, contents: req.body.contents});
-    db.collection('counter').updateOne({name:'postNumber'}, { $inc: { totalPost: +1}});
+    db.collection('post').insertOne({_id: totalPost+1,title: req.body.title, date: req.body.date, contents: req.body.contents}, console.log('저장 완료'));
+    db.collection('counter').updateOne({name:'postNumber'}, { $inc: { totalPost: +1}}, console.log('변경 완료'));
     res.redirect('/');
 })
 
@@ -48,9 +47,9 @@ app.get('/list', async (err, res) => {
 
 app.delete('/list/:id', async (req, res, err) => {
   try {
-    console.log(req.params.id)
-    const result = await db.collection('post').deleteOne({_id: req.params.id}, console.log('삭제 완료'));
-    console.log(result)
+    const postId = parseInt(req.params.id)
+    const result = await db.collection('post').deleteOne({_id: postId}, console.log('삭제 완료'));
+    db.collection('counter').updateOne({name:'postNumber'}, { $inc: { totalPost: -1}}, console.log('변경 완료'));
     res.redirect('/list')
   } catch (err) {
     console.error(err)
